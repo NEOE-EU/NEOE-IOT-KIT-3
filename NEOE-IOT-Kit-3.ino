2df6034523d3fd952b300709cecef1a838c4cf52
NEOE-EU <55393250+NEOE-EU@users.noreply.github.com>
2020-02-01 21:29:38 +0100

    Create NEOE-IOT-Kit-3.ino

---
+++ NEOE-IOT-Kit-3.ino
@@ -0,0 +1,55 @@
+/***************************************************************************
+  This is a library for the DHT22 temerature and humidity sensor.
+  Developed for the NEOE-IOT-Kit 3 "Temperature and humidity with 
+  DHT22 / AM2302"
+  www.neoe.io/...
+  
+  Developed with input and suggestions from the community. 
+  Please share your feedback, thougths and suggestions with us.
+  www.neoe.io/kontakt/
+
+  Date of last change: February 1st, 2020
+ **************************************************************************/
+ 
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <DHT.h>
+
+/* BLYNK Connection Settings */
+char auth[] = "ADD YOUR BLYNK-TOKEN HERE";
+char ssid[] = "ADD THE SSID OF YOUR WLAN HERE";
+char pass[] = "ADD THE PASSWORD OF YOUR WLAN HERE";
+
+#define DHTPIN 5        // Digital sensor is connected, e.g. D1 = PIN 5
+#define DHTTYPE DHT22   // Type of DHT-Sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+
+}
+
+void loop() {
+
+  /* Ensure that Blynk reconnects in case of lost connection */
+  if(!Blynk.connected() ) {
+    Blynk.connect();
+  }
+
+  Blynk.run(); // Initiates Blynk
+
+  /* Read data from Sensor */
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); 
+
+  /* Send data to Blynk */
+    Blynk.virtualWrite(V0, t);    // Temperatur
+    Blynk.virtualWrite(V1, h);    // Luftfeuchtigkeit
+  
+  delay(1000);
+
+}
